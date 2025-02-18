6a03ee7a4c45dde9223d97d0d50039714a8b0500
Yaminianitha19 <yaminianitha22@gmail.com>
2025-02-18 18:50:58 +0530

    navigation was created

--- website/views.py
+++ website/views.py
@@ -4,8 +4,8 @@ views = Blueprint("views", __name__)
 
 
 
-
+@views.route("/")
 @views.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", name ="Yamini")
 
